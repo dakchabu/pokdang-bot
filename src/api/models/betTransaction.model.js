5c9d3bf12c397d14f24ec57f0824d2ab18264859
Chantawat.Mod <chantawat.ambgames@gmail.com>
2022-11-26 07:17:12 +0700

    [Add] result for admin & betting for player

--- src/api/models/betTransaction.model.js
+++ src/api/models/betTransaction.model.js
@@ -3,9 +3,9 @@ const { Schema } = require('mongoose')
 
 const betTransactionSchema = new mongoose.Schema({
   userId: { type: String, required: true, index: true },
-  roundId: { type: String, required: true },
-  groupId: { type: String, required: true },
-  wallet: {
+  roundId: { type: String, required: true, index: true },
+  groupId: { type: String, required: true, index: true },
+  balance: {
     bet: {
       before: { type: Types.Decimal128, default: 0 },
       after: { type: Types.Decimal128, default: 0 },
@@ -15,13 +15,13 @@ const betTransactionSchema = new mongoose.Schema({
       after: { type: Types.Decimal128, default: 0 },
     }
   },
-  type: { type: String, required: true, default: 'BET' },
-  result: { type: Schema.Types.Mixed, default: {} },
-  createdDate: { type: Date, default: new Date() },
-  updatedDate: { type: Date, default: new Date() },
+  type: { type: String, required: true, index: true, default: 'BET' },
+  bet: { type: Schema.Types.Mixed, default: {} },
+  createdDate: { type: Date, default: new Date(), index: true },
+  updatedDate: { type: Date, default: new Date(), index: true },
 });
 
 module.exports = mongoose.main.model('BetTransaction', betTransactionSchema);
-betTransactionSchema.index({ 'playId.provider': 1, 'playId.name': 1 }, { unique: true, partialFilterExpression: { role: { $eq: 'MEMBER' } } })
-betTransactionSchema.index({ 'prefix': 1, 'bank.bankCode': 1, 'bank.accountCode': 1 }, { unique: true, partialFilterExpression: { role: { $eq: 'MEMBER' } } })
+// betTransactionSchema.index({ 'playId.provider': 1, 'playId.name': 1 }, { unique: true, partialFilterExpression: { role: { $eq: 'MEMBER' } } })
+// betTransactionSchema.index({ 'prefix': 1, 'bank.bankCode': 1, 'bank.accountCode': 1 }, { unique: true, partialFilterExpression: { role: { $eq: 'MEMBER' } } })
 
